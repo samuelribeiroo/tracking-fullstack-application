595eb02000f39b47dbe1b3bf7e07f8c88b28d8fd
samuelribeiroo <samuelribeirodev@gmail.com>
2024-12-31 23:08:25 -0300

    feat(server side): create route action

--- frontend-next/src/app/actions/route-actions.ts
+++ frontend-next/src/app/actions/route-actions.ts
@@ -1,4 +1,4 @@
-'use server'
+"use server";
 
 export async function searchDirections(source: string, destination: string) {
   const [sourceResponse, destinationResponse] = await Promise.all([
@@ -43,3 +43,56 @@ export async function searchDirections(source: string, destination: string) {
     directionsData,
   };
 }
+
+export type RouteState = {
+  error?: string;
+  success?: boolean;
+} | null;
+
+export async function createRouteAction(state: RouteState, formData: FormData) {
+  try {
+    const sourceId = formData.get("sourceId");
+    const destinationId = formData.get("destinationId");
+
+    if (!sourceId || !destinationId) return { error: 'Source ID e Destination ID são obrigatórios', success: false };
+    
+
+    const directionResponse = await fetch(
+      `http://localhost:3000/directions?originId=${sourceId}&destinationId=${destinationId}`
+    );
+
+    if (!directionResponse.ok) {
+      return { error: 'Não foi possível obter as direções da rota.', success: false };
+    }
+
+    const data = await directionResponse.json();
+
+    if (!data.routes?.[0]?.legs?.[0]) {
+      return { error: 'Dados da rota inválidos ou incompletos.', success: false };
+    }
+
+    const startAddress = data.routes[0].legs[0].start_address;
+    const endAddress = data.routes[0].legs[0].end_address;
+
+    const response = await fetch(`http://localhost:3000/routes`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: `${startAddress} - ${endAddress}`,
+        source_id: data.request.origin.place_id.replace("place_id:", ""),
+        destination_id: data.request.destination.place_id.replace("place_id:", ""),
+        startAddress,
+        endAddress,
+      }),
+    });
+
+    if (!response.ok) {
+      return { error: 'Não foi possível criar a rota.', success: false };
+    }
+
+    return { success: true, error: undefined };
+  } catch (err) {
+    return { error: 'Ocorreu um erro ao processar a rota.', success: false };
+  }
+}
+
