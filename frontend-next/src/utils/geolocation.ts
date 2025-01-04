a747cca1a0cd5485da289042880ebf413cf63b66
samuelribeiroo <samuelribeirodev@gmail.com>
2025-01-04 10:46:08 -0300

    refactor(try/catch): prefer use try catch scope

--- frontend-next/src/utils/geolocation.ts
+++ frontend-next/src/utils/geolocation.ts
@@ -1,31 +1,53 @@
-
 // Function created to generate directions data in a valid state with lat e lng to enable maps api search
-export async function geocodeAddress(address: string): Promise<{ lat: number; lng: number }> {
+export async function geocodeAddress(
+  address: string
+): Promise<{ lat: number; lng: number }> {
   const geocoder = new google.maps.Geocoder();
-  return new Promise((resolve, reject) => {
-    geocoder.geocode({ address }, (results, status) => {
-      if (status === "OK" && results && results.length > 0 && results[0].geometry.location) {
-        const { lat, lng } = results[0].geometry.location.toJSON();
-        resolve({ lat, lng });
-      } else {
-        reject(`Geocoding failed: ${status}`);
+
+  try {
+    const data = await new Promise<google.maps.GeocoderResult[]>(
+      (resolve, reject) => {
+        geocoder.geocode({ address }, (results, status) => {
+          if (status === "OK" && results && results.length > 0) {
+            resolve(results);
+          } else {
+            reject(new Error(`Geocoding failed: ${status}`));
+          }
+        });
       }
-    });
-  });
+    );
+
+    const { lat, lng } = data[0].geometry.location.toJSON();
+
+    return { lat, lng };
+  } catch (error) {
+    throw new Error(
+      error instanceof Error
+        ? error.message
+        : "Erro desconhecido ao tentar localizar."
+    );
+  }
 }
 
 export async function getCurrentPosition(
   options?: PositionOptions
 ): Promise<{ lat: number; lng: number }> {
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(
-      (position) =>
-        resolve({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        }),
-      (error) => reject(error),
-      options
+  try {
+    const position = await new Promise<GeolocationPosition>(
+      (resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject, options);
+      }
     );
-  });
+
+    return {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    };
+  } catch (error) {
+    throw new Error(
+      error instanceof GeolocationPositionError
+        ? error.message
+        : "Falha ao obter a posição atual"
+    );
+  }
 }
