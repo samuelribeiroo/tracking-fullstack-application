9b7e7971e2ae77aebf672818d9745a318337736e
samuelribeiroo <samuelribeirodev@gmail.com>
2025-01-06 18:58:59 -0300

    feat(driver route tracking): process and start routes

--- backend-api/src/routes/routes.controller.ts
+++ backend-api/src/routes/routes.controller.ts
@@ -1,18 +1,47 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 // biome-ignore lint/style/useImportType: <explanation>
 import { RoutesService } from './routes.service';
 // biome-ignore lint/style/useImportType: <explanation>
 import { CreateRouteDto } from './dto/create-route.dto';
 // biome-ignore lint/style/useImportType: <explanation>
 import { UpdateRouteDto } from './dto/update-route.dto';
+import { RoutesDriverService } from './routes-driver/routes-driver.service';
 
 @Controller('routes')
 export class RoutesController {
-  constructor(private readonly routesService: RoutesService) {}
+  constructor(
+    private readonly routesService: RoutesService,
+    private readonly routeDriverService: RoutesDriverService,
+  ) {}
 
   @Post()
   create(@Body() createRouteDto: CreateRouteDto) {
     return this.routesService.create(createRouteDto);
+  }
+
+  @Post(':id/process-route')
+  processRoute(
+    @Param('id') id: string,
+    @Body() data: { lat: number; lng: number },
+  ) {
+    return this.routeDriverService.processRoute({
+      route_id: id,
+      lat: data.lat,
+      lng: data.lng,
+    });
+  }
+
+  @Post(':id/start')
+  startRoute(@Param('id') id: string) {
+    return this.routesService.startRoute(id);
   }
 
   @Get()
--- backend-api/src/routes/routes.service.ts
+++ backend-api/src/routes/routes.service.ts
@@ -18,13 +18,22 @@ export class RoutesService {
   async create({ source_id, name, destination_id }: CreateRouteDto) {
     try {
       const sourceId = source_id ? String(source_id).trim() : null;
-      const destinationId = destination_id ? String(destination_id).trim() : null;
+      const destinationId = destination_id
+        ? String(destination_id).trim()
+        : null;
 
-      if (!sourceId || !destinationId) throw new HttpException( 'Os IDs de origem e destino são obrigatórios!', HttpStatus.BAD_REQUEST);
+      if (!sourceId || !destinationId)
+        throw new HttpException(
+          'Os IDs de origem e destino são obrigatórios!',
+          HttpStatus.BAD_REQUEST,
+        );
 
-      const { available_travel_modes, geocoded_waypoints, routes, request } = 
-      await this.directionsService.getDirectionsService(sourceId, destinationId);
-    
+      const { available_travel_modes, geocoded_waypoints, routes, request } =
+        await this.directionsService.getDirectionsService(
+          sourceId,
+          destinationId,
+        );
+
       const legs = routes[0].legs[0];
 
       const createRoute = await this.prismaService.route.create({
@@ -61,8 +70,34 @@ export class RoutesService {
       return createRoute;
     } catch (error) {
       if (error instanceof HttpException) throw error;
-      
-      throw new HttpException('Erro interno no servidor ao criar a rota', HttpStatus.INTERNAL_SERVER_ERROR);
+
+      throw new HttpException(
+        'Erro interno no servidor ao criar a rota',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async startRoute(id: string): Promise<{ sucess: Boolean }> {
+    try {
+      const findUniqueRoute = await this.prismaService.route.findFirstOrThrow({
+        where: { id },
+      });
+
+      if (!findUniqueRoute)
+        throw new HttpException(
+          'Rota solicitada não encontrada',
+          HttpStatus.NOT_FOUND,
+        );
+
+      return { sucess: true };
+    } catch (error) {
+      if (error instanceof HttpException) throw error;
+
+      throw new HttpException(
+        'Erro interno no servidor ao criar a rota',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 
