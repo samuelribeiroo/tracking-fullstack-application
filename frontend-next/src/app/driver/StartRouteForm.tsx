4d97dfc57b13b2337ccb3d1c2ca9e2c5cb85ac22
samuelribeiroo <samuelribeirodev@gmail.com>
2025-01-06 09:59:11 -0300

    add(start route): actions and state changes

--- frontend-next/src/app/driver/StartRouteForm.tsx
+++ frontend-next/src/app/driver/StartRouteForm.tsx
@@ -1,11 +1,21 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useActionState } from "react";
+import { startRouteAction } from "./start-route-action";
+import { StartRouteState } from "@/src/utils/models";
+import useToastNotifications from "@/src/hooks/useToastNotification";
 
 export default function StartRouteForm({ children }: PropsWithChildren) {
+  const [state, formAction] = useActionState<StartRouteState, FormData>(
+    startRouteAction,
+    {}
+  );
+
+  useToastNotifications(state)
+
   return (
     <>
-    <form className="flex flex-col gap-2">
-      {children}
-    </form>
+      <form className="flex flex-col gap-2" action={formAction}>
+        {children}
+      </form>
     </>
-  )
+  );
 }
