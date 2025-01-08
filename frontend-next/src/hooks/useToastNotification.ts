7862096ed09076ecd1116b6f01c1c990b41adda0
samuelribeiroo <samuelribeirodev@gmail.com>
2025-01-08 14:13:46 -0300

    update(toast message): props to display property message

--- frontend-next/src/hooks/useToastNotification.ts
+++ frontend-next/src/hooks/useToastNotification.ts
@@ -2,21 +2,31 @@ import { useEffect } from "react";
 import { RouteState } from "../app/actions/route-actions";
 import { Zoom, toast } from "react-toastify";
 
-export default function useToastNotifications(state: RouteState) {
+interface UseToastNotificationsProps {
+  state: RouteState;
+  successMessage?: string; 
+  errorMessage?: string;   
+}
+
+export default function useToastNotifications({
+  state,
+  successMessage = "Rota adicionada com sucesso!",
+  errorMessage = "Erro ao processar a rota."
+}: UseToastNotificationsProps) {
   useEffect(() => {
     if (!state) return;
 
     if (state.success) {
-      toast.success("Rota adicionada com sucesso!", {
+      toast.success(successMessage, {
         transition: Zoom,
         theme: "colored",
       });
     }
     if (state.error) {
-      toast.error(`Erro: ${state.error}`, {
+      toast.error(errorMessage, {
         transition: Zoom,
         theme: "colored",
       });
     }
-  }, [state]);
+  }, [state, successMessage, errorMessage]);
 }
