10c559c06be210765574c31136559d1469bf305a
samuelribeiroo <samuelribeirodev@gmail.com>
2025-01-12 18:05:18 -0300

    add(admin page): route tracked in admin page

--- frontend-next/src/app/admin/page.tsx
+++ frontend-next/src/app/admin/page.tsx
@@ -1,14 +1,21 @@
-'use client'
+"use client";
 
-import Map from "@/src/components/ui/map";
-import { useMap } from "@/src/hooks/useMap";
-import { useEffect, useRef } from "react";
-
+import useTrackRoute from "@/src/hooks/useTrackingRoute";
+import MapDriver from "../driver/MapDriver";
 
 export default function AdminPage() {
-  const mapContainerRef = useRef<HTMLDivElement>(null);
-  useMap(mapContainerRef);
-  
-  return <Map ref={mapContainerRef}/>
-  
+  const { routeDetails, routeId } = useTrackRoute();
+
+  const start_location =
+    routeDetails?.directions?.routes[0]?.legs[0]?.start_location;
+  const end_location =
+    routeDetails?.directions?.routes[0]?.legs[0]?.end_location;
+
+  return (
+    <MapDriver
+      route_id={routeId}
+      start_location={start_location}
+      end_location={end_location}
+    />
+  );
 }
