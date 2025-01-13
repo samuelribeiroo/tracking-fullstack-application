2fa69bacd803f1ed985337568144a546e9bc62ba
samuelribeiroo <samuelribeirodev@gmail.com>
2025-01-13 13:46:30 -0300

    feat(new methods): calculate freight and get single route

--- golang-simulator/internal/route.go
+++ golang-simulator/internal/route.go
@@ -1,6 +1,9 @@
 package internal
 
 import (
+	"context"
+	"math"
+
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -18,12 +21,23 @@ type Route struct {
 	FreightPrice float64
 }
 
-/* Responsible to create a route and save it at db  */
+type FreighService struct {}
+
+func (fs *FreighService) CalculateFreightService(distance int) float64 {
+	return math.Round((float64(distance) * 0.15 + 0.49 * 100)) / 100
+}
+
+
 type RouteService struct {
 	mongo *mongo.Client
+	freighService FreighService
 }
 
 func (rs *RouteService) CreateRoute(route Route) (Route, error) {
+	ctx := context.TODO()
+
+	route.FreightPrice = rs.freighService.CalculateFreightService(route.Distance)
+	
 	update := bson.M{
 		"$set": bson.M{
 			"distance": route.Distance,
@@ -35,11 +49,22 @@ func (rs *RouteService) CreateRoute(route Route) (Route, error) {
 	filter := bson.M{"_id": route.ID}
 	opts := options.Update().SetUpsert(true)
 
-	_, err := rs.mongo.Database("routes").Collection("routes_c").UpdateOne(nil, filter, update, opts)
+	_, err := rs.mongo.Database("routes").Collection("routes_c").UpdateOne(ctx, filter, update, opts)
 
 	if err != nil {
 		return Route{}, err
 	}
 
   return route, err
+}
+
+func (rs *RouteService) GetRoute (id string) (Route, error) {
+	ctx := context.TODO()
+	
+	var route Route
+	filter := bson.M{"_id": id}
+	
+	err := rs.mongo.Database("routes").Collection("routes_c").FindOne(ctx, filter).Decode(&route)
+
+	return route, err
 }
