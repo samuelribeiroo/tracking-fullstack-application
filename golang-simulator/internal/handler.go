b63c0be17b59911e68be18f16fbfb1748c0bd158
samuelribeiroo <samuelribeirodev@gmail.com>
2025-01-15 11:14:17 -0300

    add(handler): handler route methods

---
+++ golang-simulator/internal/handler.go
@@ -0,0 +1,77 @@
+package internal
+
+import (
+	
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type RouteCreatedEvent struct {
+	EventName string `json:"event"`
+	RouteId   string  `json:"id"`
+	Distance int      `json:"distance"`
+	Directions []Directions `json:"directions"`
+}
+
+func NewRouteCreatedEvent(routeID string, distance int, directions []Directions) *RouteCreatedEvent {
+	return &RouteCreatedEvent{
+		EventName: "RouteCreated",
+		RouteId:  routeID,
+		Distance: distance,
+		Directions: directions,
+	}
+}
+
+type FreightCalculatedEvent struct {
+	EventName string  `json:"event"`
+	RouteID   string  `json:"route_id"`
+	Amount    float64 `json:"amount"`
+}
+
+func NewFreightCalculatedEvent(routeID string, amount float64) *FreightCalculatedEvent  {
+	return &FreightCalculatedEvent{
+		EventName: "RouteCreated",
+		RouteID: routeID,
+		Amount: amount,
+	}
+}
+
+type DeliveryStartedEvent struct {
+	EventName string  `json:"event"`
+	RouteID   string  `json:"route_id"`
+}
+
+func NewDeliveryStartedEvent(routeID string) *DeliveryStartedEvent {
+	return &DeliveryStartedEvent{
+		EventName: "RouteCreated",
+		RouteID: routeID,
+	}
+}
+
+type DriverMovedEvent struct {
+	EventName string  `json:"event"`
+	RouteID   string  `json:"route_id"`
+	Lat       float64 `json:"lat"`
+	Lng       float64 `json:"lng"`
+}
+
+func NewDriverMovedEvent(routeID string, lat float64, lng float64) *DriverMovedEvent {
+	return &DriverMovedEvent{
+		EventName: "DriverMoved",
+		RouteID: routeID,
+		Lat: lat,
+		Lng: lng,
+	}
+}
+
+func RouteCreatedHandler(event *RouteCreatedEvent, routeService *RouteService, mongoClient *mongo.Client) (*FreightCalculatedEvent, error) {
+	route := NewRoute(event.RouteId, event.Distance, event.Directions)
+	routeCreated, err := routeService.CreateRoute(route)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return NewFreightCalculatedEvent(routeCreated.ID, routeCreated.FreightPrice), nil
+}
+
+/* Falta iniciar: DeliveryStartedHandler */
